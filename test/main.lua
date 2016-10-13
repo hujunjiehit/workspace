f02194cb7758f0841c33cb338af2455ab468e071
hujunjie <1006879674@qq.com>
2016-10-13 20:52:00 +0800

    新版本的下单

--- test/main.lua
+++ test/main.lua
@@ -5,9 +5,22 @@ function main()
 	-- body
 	path = getSDCardPath();
 	
-	nLog("hello,world");
-	x, y = findImageInRegionFuzzy("私教团购订单.png",80, 127,51,648,141,0x22ac39);
-	nLog("x = "..x.."   y = "..y);
+	deviceBrand = getDeviceBrand();
+	deviceModel = getDeviceModel(); 
+	
+	nLog("deviceBrand = "..deviceBrand.."   deviceModel = "..deviceModel);
+	
+	if deviceModel == "Micromax AO5510" then
+		nLog("hello,".."大神f2");
+	else
+		nLog("hello,".."S6-nt");
+	end
+	
+		
+	
+	--nLog("hello,world");
+	--x, y = findImageInRegionFuzzy("more_info_大神f2.png", 90, 471,  149,  717, 1180, 0xffffff);
+	--nLog("x = "..x.."   y = "..y);
 	
 end
 
