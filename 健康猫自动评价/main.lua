9107a6fe3ddfffb6951c1783d989407ee2387bc3
hujunjie <1006879674@qq.com>
2016-11-28 19:57:50 +0800

    安卓新版本评价

--- 健康猫自动评价/main.lua
+++ 健康猫自动评价/main.lua
@@ -108,8 +108,7 @@ function logout()
 	mSleep(1000)
 	tap(320,1020);	--进入设置
 	mSleep(1000)
-	tap(350,799);	--点击退出登录
-	
+	tap(353,836);	--点击退出登录
 
 	deviceModel = getDeviceModel();
 	write_to_log(deviceModel);
@@ -120,12 +119,10 @@ function logout()
 		mSleep(1000)
 		tap(610,720);	--点击确定
 	end
-
-	
 	
 	repeat
 		mSleep(1000)
-	until getColor(501,798) ~= 0x663434  --健康猫logo color
+	until isColor(592,103,0x22ac39,85)
 	mSleep(1000)
 	nLog(userName.."退出登录");
 end
@@ -133,128 +130,6 @@ end
 function beforeUserExit()
     nLog("before user exit");
 	switchTSInputMethod(false);
-end
-
-function startToPingjia(begin)
-	
-	flag_count = 0; 
-	
-	--先判断需不需要评价，通过找颜色，如果不需要直接返回
-	m,n = findColorInRegionFuzzy(0x33c774,80,0,50,720,1280);
-	nLog(m.."----"..n)
-	if m == -1 and n == -1 then
-		--当前页面没有需要评价的
-		return flag_count;
-	end
-	
-	
-	switchTSInputMethod(true);
-	for index = begin,7 do	
-		nLog("index = "..index.."   y  = "..tostring(218+155*(index-1)));
-		y = 207+142*(index-1);
-		tap(350,218+155*(index-1));	
-		
-		repeat
-			mSleep(1000)
-			nLog("正在加载课程详情页 wait...");
-		until getColor(82,194) ~= 0xffffff	--数据加载中的颜色
-		--此处有可能网络出错
-		
-		mSleep(1000);
-		
-		if getColor(282, 1231) == 0x33c774 then  --可以评价
-			tap(282,1231)
-			mSleep(2000);
-			
-			tap(457,476);	--点击五星   **************兼容性处理
-			mSleep(500);
-			
-			tap(457,453);	--点击五星   **************兼容性处理，兼容模拟器
-			mSleep(500);
-			
-			tap(457,466);	--点击五星
-			mSleep(500);
-			
-			tap(333,627);	--点击输入框，获取焦点
-			mSleep(2000);
-			--inputText("很好非常好");
-			math.randomseed(getRndNum()) -- 随机种子初始化真随机数
-			num = math.random(1, words_count) -- 随机获取一个1-100之间的数字
-			inputText(words[num]);
-				
-			mSleep(2000);
-			
-			tap(351,1231);	--点击提交评价
-			repeat
-				nLog("wait for...");
-				mSleep(1000)
-				color_next = getColor(220,1232)		--点击之后，该点的颜色
-			until color_next == 0x33c774 or color_next == 0xf2f2f2
-			--根据color_next判断下一步动作
-			--1.color_next == 0x33c774 未跳转，还在当前页面，表示网络出错
-			--2.color_next == 0xf2f2f2 跳转成功，表示评价成功
-			
-			mSleep(2000)
-			color_next = getColor(220,1232)
-			if color_next == 0x33c774 then
-				--评价失败，可能网络出错，此时直接返回到列表页
-				nLog("评价失败，可能网络异常。");
-				os.execute("input keyevent 4");
-				mSleep(2000)
-				os.execute("input keyevent 4");
-				mSleep(2000)
-			elseif color_next == 0xf2f2f2 then
-				--评价成功
-				nLog("评价成功。");
-				flag_count = flag_count + 1;
-				os.execute("input keyevent 4");
-				mSleep(2000)
-			end
-		else	
-			--已经评价过了，直接返回
-			os.execute("input keyevent 4");
-			mSleep(2000)
-		end
-		
-	end
-	nLog("成功进行了"..flag_count.."条评价");
-	switchTSInputMethod(false);
-	
-	return flag_count;
-end
-
-function doTheWork_pingjia(...)
-	-- body
-		for i = index,#data do
-		info = strSplit(data[i],",");
-		userName = info[1];
-		passWord = info[2];
-		nLog("i = "..i.."   userName = "..userName.."   passWord = "..passWord);
-		mSleep(500)
-		
-		login(userName,passWord);
-		
-		mSleep(1000);
-		
-		gotoPingjiaPage();  --跳转到评价页面
-		
-		num1 = startToPingjia(1);
-		nLog("num1 = "..num1);
-		
-		pull_the_screen(320,800,-600);
-		mSleep(1000)
-		
-		num2 = startToPingjia(3);
-		nLog("num2 = "..num2);
-		
-		nLog("帐号"..userName.."成功进行了"..num1+num2.."条评价");
-		write_to_log("帐号"..userName.."成功进行了"..num1+num2.."条评价")
-		
-		mSleep(1000)
-		os.execute("input keyevent 4")
-		mSleep(2000)
-		logout();
-	end
 end
 
 
@@ -273,20 +148,16 @@ function startToPingjia_special(begin)
 			repeat
 				mSleep(1000)
 				nLog("正在加载课程详情页 wait...");
-			until getColor(82,194) ~= 0xffffff	--数据加载中的颜色
-			--此处有可能网络出错
+			until isColor(419,682,0xf2f2f2,95)
+			mSleep(500);
 			
-			mSleep(1000);
-		
-			if getColor(282, 1231) == 0x33c774 then  --可以评价
-				tap(282,1231)
-				mSleep(2000);
+			if isColor(282, 1231,0x33c774,85) then  --可以评价
 				
 				tap(333,627);	--点击输入框，获取焦点
 				mSleep(1000);
 				--inputText("很好非常好");
 				
-				math.randomseed(getRndNum()) -- 随机种子初始化真随机数
+				math.randomseed(getRndNum()) -- 随机种子初始化随机数
 				num = math.random(1, words_count) -- 随机获取一个1-100之间的数字
 				inputText(words[num]);
 				
@@ -295,33 +166,20 @@ function startToPingjia_special(begin)
 				tap(351,1231);	--点击提交评价
 				repeat
 					nLog("wait for...");
-					mSleep(1000)
-					color_next = getColor(220,1232)		--点击之后，该点的颜色
-				until color_next == 0x33c774 or color_next == 0xf2f2f2
-				--根据color_next判断下一步动作
-				--1.color_next == 0x33c774 未跳转，还在当前页面，表示网络出错
-				--2.color_next == 0xf2f2f2 跳转成功，表示评价成功
+					mSleep(2000)
+				until isColor(386,668,0xc2c2c2,85) == false
+				mSleep(1000);
 				
-				mSleep(2000)
-				color_next = getColor(220,1232)
-				if color_next == 0x33c774 then
-					--评价失败，可能网络出错，此时直接返回到列表页
-					nLog("评价失败，可能网络异常。");
+				if isColor(282, 1231,0x33c774,85) then --已经评价过了
 					os.execute("input keyevent 4");
-					mSleep(2000)
-					os.execute("input keyevent 4");
-					mSleep(2000)
-				elseif color_next == 0xf2f2f2 then
-					--评价成功
-					nLog("评价成功。");
-					flag_count = flag_count + 1;
-					os.execute("input keyevent 4");
-					mSleep(2000)
+					mSleep(1000);
+					pull_the_screen(320,800,1000);
+					mSleep(2000);
 				end
 			else	
 				--已经评价过了，直接返回
 				os.execute("input keyevent 4");
-				mSleep(2000)
+				mSleep(1000)
 			end
 		end
 	until m == -1 and n == -1
@@ -334,7 +192,7 @@ end
 
 function doTheWork_pingjia_special(...)
 		-- body
-		for i = index,#data do
+	for i = index,#data do
 		info = strSplit(data[i],",");
 		userName = info[1];
 		passWord = info[2];
@@ -428,7 +286,6 @@ function main()
 	init(0)
 	initLog("test", 0);	--把 0 换成 1 即生成形似 test_1397679553.log 的日志文件 
 	wLog("test","[DATE] init log OK!!!"); 
-	showFloatButton(false);
 	
 	write_to_log("\n\n\n\n脚本开始时间:"..os.date("%c"))
 	
@@ -445,7 +302,7 @@ function main()
 	
 	UINew({titles="我的脚本",okname="开始",cancelname="取消",config="UIconfig.dat"})
 	UILabel("脚本功能选择：",15,"left","255,0,0",-1,0) --宽度写-1为一行，自定义宽度可写其他数值
-	UIRadio("mode","自动评价功能,评价查漏版本,管理评价语,手动添加帐号")
+	UIRadio("mode","新版本自动评价,管理评价语,手动添加帐号")
 	UILabel("请选择从哪一个帐号开始依次往下评价：",15,"left","255,0,0",-1,0) --宽度写-1为一行，自定义宽度可写其他数值
 	UICombo("choice_name",str)--可选参数如果写部分的话，该参数前的所有参数都必须需要填写，否则会
 	
@@ -479,7 +336,7 @@ function main()
 				end
 			end
 		until false
-	elseif mode == "评价查漏版本" then
+	elseif mode == "新版本自动评价" then
 		if isFileExist(path.."/TouchSprite/res/评价语.txt") == false then --存在返回true，不存在返回false
 			writeFileString(path.."/TouchSprite/res/评价语.txt","很好非常好\n");
 		end
@@ -490,13 +347,6 @@ function main()
 		
 	elseif mode == "管理评价语" then
 		manage_the_pingjia_words();
-	else
-		if isFileExist(path.."/TouchSprite/res/评价语.txt") == false then --存在返回true，不存在返回false
-			writeFileString(path.."/TouchSprite/res/评价语.txt","很好非常好\n");
-		end
-		words = readFile(path.."/TouchSprite/res/评价语.txt");
-		words_count = #words;
-		doTheWork_pingjia();
 	end
 	setScreenScale(false, 720, 1280)
 	closeLog("test");  --关闭日志
--- 闷声发大财/main.lua
+++ 闷声发大财/main.lua
@@ -1,17 +1,81 @@
 require "TSLib"
 
+function pull_the_screen(x,y,dy)
+	moveTo(x,y,x,y+dy)
+end
+
+function startToPingjia_special(begin)
+	
+	flag_count = 0; 
+	
+	switchTSInputMethod(true);
+	
+	repeat
+		mSleep(1000)
+		m,n = findColorInRegionFuzzy(0x33c774,80,0,50,720,1270);
+		if m ~= -1 and n ~= -1 then
+			tap(m+2,n+2);
+			
+			repeat
+				mSleep(1000)
+				nLog("正在加载课程详情页 wait...");
+			until isColor(419,682,0xf2f2f2,95)
+			mSleep(500);
+			
+			if isColor(282, 1231,0x33c774,85) then  --可以评价
+				
+				tap(333,627);	--点击输入框，获取焦点
+				mSleep(1000);
+				--inputText("很好非常好");
+				
+				math.randomseed(getRndNum()) -- 随机种子初始化随机数
+				num = math.random(1, words_count) -- 随机获取一个1-100之间的数字
+				inputText(words[num]);
+				
+				mSleep(1000);
+				
+				tap(351,1231);	--点击提交评价
+				repeat
+					nLog("wait for...");
+					mSleep(2000)
+				until isColor(386,668,0xc2c2c2,85) == false
+				mSleep(1000);
+				
+				if isColor(282, 1231,0x33c774,85) then --已经评价过了
+					os.execute("input keyevent 4");
+					mSleep(1000);
+					pull_the_screen(320,800,1000);
+					mSleep(2000);
+				end
+			else	
+				--已经评价过了，直接返回
+				os.execute("input keyevent 4");
+				mSleep(1000)
+			end
+		end
+	until m == -1 and n == -1
+	
+	nLog("成功进行了"..flag_count.."条评价");
+	switchTSInputMethod(false);
+	
+	return flag_count;
+end
+
+
 function main(...)
 	-- body
-	init("0", 0);  --竖屏
+	init("0");  --竖屏
 	initLog("脚本宣传记录", 0);	--把 0 换成 1 即生成形似 test_1397679553.log 的日志文件 
-	showFloatButton(false);
-	
-	sysver = getOSVer();    --获取系统版本
-	sysint = tonumber(string.sub(sysver, 1, 1)..string.sub(sysver, 3,3)..string.sub(sysver, 5, 5)); 
-	
-	toast(sysint,1)
-	
-	showFloatButton(true);
+
+	path = getSDCardPath();
+	if isFileExist(path.."/TouchSprite/res/评价语.txt") == false then --存在返回true，不存在返回false
+		writeFileString(path.."/TouchSprite/res/评价语.txt","很好非常好\n");
+	end
+	words = readFile(path.."/TouchSprite/res/评价语.txt");
+	words_count = #words;
+		
+	startToPingjia_special(1);
+
 	closeLog("脚本宣传记录");  --关闭日志
 end
 
